fea317ab2330c133bf3b143891878e037c902ff3
Nik <nik@khilnani.org>
2014-06-18 00:06:36 -0400

    Fixed formatting

--- src/modules/validator.js
+++ src/modules/validator.js
@@ -1,41 +1,34 @@
 //--------------------------------------------
-
 var log = require("dysf.utils").logger,
-	illegals = require("../config/config.illegals"),
-	v = exports;
-	
+  illegals = require("../config/config.illegals"),
+  v = exports;
+
 //--------------------------------------------
 
+v.validate = function (code, guid) {
+  guid = guid || (new Date()).getTime();
+  log.debug("isValid(): Start. guid: " + guid + " code: " + code);
 
-v.validate = function (code, guid)
-{
-	guid = guid || (new Date()).getTime();
-	log.debug("isValid(): Start. guid: " + guid + " code: " + code);
-	
-	code = code.toLowerCase();
-	code = code.replace(/[^a-z0-9]/g, '');
+  code = code.toLowerCase();
+  code = code.replace(/[^a-z0-9]/g, '');
 
-	log.trace("isValid(): Condensed. guid: " + guid + " code: " + code);
+  log.trace("isValid(): Condensed. guid: " + guid + " code: " + code);
 
+  var usage = [];
 
-	var usage = [];
-	
-	log.trace("illegals.list: " + illegals.list);
-	illegals.list.forEach( function(val) {
-		//log.trace("Val: " + val);
-		if(code.indexOf(val.toLowerCase()) > -1)
-		{
-			usage.push( val );
-			log.warn("isValid(): guid: " + guid + " illegal found: " + val);
-		}
-		
-	});
-	
+  log.trace("illegals.list: " + illegals.list);
+  illegals.list.forEach(function (val) {
+    //log.trace("Val: " + val);
+    if (code.indexOf(val.toLowerCase()) > -1) {
+      usage.push(val);
+      log.warn("isValid(): guid: " + guid + " illegal found: " + val);
+    }
 
-	log.debug("isValid(): Ended. guid: " + guid + " usage: " + usage.length);
-	
-	return usage;
+  });
+
+  log.debug("isValid(): Ended. guid: " + guid + " usage: " + usage.length);
+
+  return usage;
 }
-
 
 //--------------------------------------------
